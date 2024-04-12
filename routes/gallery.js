a01febc284886dfb3e6ba2587b91da79647191e6
Diana Moretti <morettidn@gmail.com>
2024-04-12 10:16:22 +0200

    adding user signup

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const db = require("../data/database");
 
@@ -10,6 +11,25 @@ router.get("/", async function (req, res) {
 
 router.get("/signup", async function (req, res) {
   res.render("signup");
+});
+
+router.post("/signup", async function (req, res) {
+  const userEnteredData = req.body;
+  const userName = userEnteredData.username;
+  const userEmail = userEnteredData.email;
+  const userPassword = userEnteredData.password;
+
+  const hashedPassword = await bcrypt.hash(userPassword, 12);
+
+  const user = {
+    username: userName,
+    email: userEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
 });
 
 router.get("/login", async function (req, res) {
