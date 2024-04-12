1c221393389e43be90e7472acbf4946649e0a1d8
Diana Moretti <morettidn@gmail.com>
2024-04-12 10:54:03 +0200

    adding signup form validation

--- routes/gallery.js
+++ routes/gallery.js
@@ -17,7 +17,30 @@ router.post("/signup", async function (req, res) {
   const userEnteredData = req.body;
   const userName = userEnteredData.username;
   const userEmail = userEnteredData.email;
+  const userConfirmEmail = userEnteredData["confirm-email"];
   const userPassword = userEnteredData.password;
+
+  if (
+    !userName ||
+    !userEmail ||
+    !userPassword ||
+    userPassword.trim().length < 6 ||
+    userEmail !== userConfirmEmail ||
+    !userEmail.includes("@")
+  ) {
+    console.log("incorrect data");
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: userEmail });
+
+  if (existingUser) {
+    console.log("user already exists");
+    return res.redirect("/signup");
+  }
 
   const hashedPassword = await bcrypt.hash(userPassword, 12);
 
